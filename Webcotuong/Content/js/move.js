9a6643a7882e805e07a36ed519e15b74e96420c4
phunghoang3b <nguyenhoangkhai492000@gmail.com>
2021-07-16 16:52:38 +0700

    #dang chinh con tot

--- Webcotuong/Content/js/move.js
+++ Webcotuong/Content/js/move.js
@@ -1,59 +1,68 @@
-﻿function getMove(name, kind, Position){
-    var accept_Position = new Array();
+﻿function getMove(name, kind, position){
+    var accept_position = new Array();
 
-    var x = Number.parseInt(Position.x);
-    var y = Number.parseInt(Position.y);
+    var x = Number.parseInt(position.x);
+    var y = Number.parseInt(position.y);
     
     if(name == "tot_do" && kind == "r"){
-        var default_Position = new Array();
-
-        default_Position.push(
-            new Position(x, y-68)           
-        );
-
-        default_Position.forEach(e => {
-            if(e.y < 392 && e.x < 773 && e.x > 229) {
-                accept_Position.push(e);
-                accept_Position.push(new Position(x - 68, y));
-                accept_Position.push(new Position(x + 68, y));
+        var default_position = new Array();
+        if (y > 120) {
+            default_position.push(
+                new Position(x, y - 68)
+            );
+        }
+        if (x != 773) {
+            default_position.push(
+                new Position(x + 68, y)
+            );
+        }
+        if (x != 229) {
+            default_position.push(
+                new Position(x - 68, y)
+            );
+        }
+        default_position.forEach(e => {
+            if (e.y < 392 && e.x < 773 && e.x > 229 && e.y != 120) {
+                accept_position.push(e);
+                accept_position.push(new Position(x - 68, y));
+                accept_position.push(new Position(x + 68, y));
             }
-            else if (e.y < 392 && e.x < 773)
+            else if (e.y < 392 && e.x != 773)
             {
-                accept_Position.push(e);
-                accept_Position.push(new Position(x + 68, y));
+                accept_position.push(e);
+                accept_position.push(new Position(x + 68, y));
             }
-            else if (e.y < 392 && e.x > 229)
+            if (e.y < 392 && e.x != 229)
             {
-                accept_Position.push(e);
-      
-                accept_Position.push(new Position(x - 68, y));
+                accept_position.push(e);      
+                accept_position.push(new Position(x - 68, y));
             }
             else{
-                accept_Position.push(e);
+                accept_position.push(e);
             }
         });
-        return accept_Position;
+        return accept_position;
     }
     
     if (name == "voi_do1" && kind == "r") {
-        var default_Position = new Array();
-        default_Position.push(
+        var default_position = new Array();
+        default_position.push(
             new Position(x - 136, y - 136),
             new Position(x + 136, y - 136),
             new Position(x + 136, y + 136),
             new Position(x - 136, y + 136)
         );
-        default_Position.forEach(e => {
+        default_position.forEach(e => {
             if (e.x >= 229 && e.y <= 732 && e.y > 392) {
-                accept_Position.push(e);
+                accept_position.push(e);
             }
         });
-        return accept_Position;
+        return accept_position;
     }
 
     if (name == "ma_do1" && kind == "r") {
-        var default_Position = new Array();
-        default_Position.push(
+        var default_position = new Array();
+        default_position.push(
             new Position(x - 68, y - 136),
             new Position(x + 68, y - 136),
             new Position(x - 136, y - 68),
@@ -63,12 +72,12 @@
             new Position(x - 68, y + 136),
             new Position(x + 68, y + 136),
         );
-        default_Position.forEach(e => {
+        default_position.forEach(e => {
             //if (e.x >= 229 && e.y <= 732 && e.x <=773) {
-            //    accept_Position.push(e);
+            //    accept_position.push(e);
             //}
             if (checkCo(e.x + 68, e.y) == true /* && checkCo(e.x + 68, e.y) != ""*/) {
-                accept_Position.push(new Position(x - 68, y - 136),
+                accept_position.push(new Position(x - 68, y - 136),
                     new Position(x + 68, y - 136),
                    
 
@@ -78,77 +87,177 @@
             }
            
         });
-        return accept_Position;
+        return accept_position;
     }
 
     if (name == "tuong_do" && kind == "r") {
-        var default_Position = new Array();
-        default_Position.push(
+        var default_position = new Array();
+        default_position.push(
             new Position(x - 68, y),
             new Position(x + 68, y),
             new Position(x, y - 68),
             new Position(x, y + 68)
         );
-        default_Position.forEach(e => {
+        default_position.forEach(e => {
             if (e.y <= 732 && e.x >= 433 && e.x <= 569 && e.y >= 596) {
-                accept_Position.push(e);
+                accept_position.push(e);
             }
         });
-        return accept_Position;
+        return accept_position;
     }
 
     if (name == "si_do1" && kind == "r") {
-        var default_Position = new Array();
-        default_Position.push(
+        var default_position = new Array();
+        default_position.push(
             new Position(x - 68, y - 68),
             new Position(x + 68, y - 68),
             new Position(x - 68, y + 68),
             new Position(x + 68, y + 68)
         );
-        default_Position.forEach(e => {
+        default_position.forEach(e => {
             if (e.y <= 732 && e.x >= 433 && e.y >= 596 && e.x <= 569) {
-                accept_Position.push(e);
+                accept_position.push(e);
             }
         });
-        return accept_Position;
+        return accept_position;
+    }
+
+    if (name == "xe_do1" && kind == "r") {
+        var default_position = new Array();
+        default_position.push(
+            //dọc đi lên 
+            new Position(x, y - 68),
+            new Position(x, y - 136),
+            new Position(x, y - 204),
+            new Position(x, y - 272),
+            new Position(x, y - 340),
+            new Position(x, y - 408),
+            new Position(x, y - 476),
+            new Position(x, y - 544),
+            new Position(x, y - 612),
+            //dọc đi xuống
+            new Position(x, y + 68),
+            new Position(x, y + 136),
+            new Position(x, y + 204),
+            new Position(x, y + 272),
+            new Position(x, y + 340),
+            new Position(x, y + 408),
+            new Position(x, y + 476),
+            new Position(x, y + 544),
+            new Position(x, y + 612),
+            //ngang qua trái
+            new Position(x - 68, y),
+            new Position(x - 136, y),
+            new Position(x - 204, y),
+            new Position(x - 272, y),
+            new Position(x - 340, y),
+            new Position(x - 408, y),
+            new Position(x - 476, y),
+            new Position(x - 544, y),
+            //ngang qua phải
+            new Position(x + 68, y),
+            new Position(x + 136, y),
+            new Position(x + 204, y),
+            new Position(x + 272, y),
+            new Position(x + 340, y),
+            new Position(x + 408, y),
+            new Position(x + 476, y),
+            new Position(x + 544, y)
+        );
+        default_position.forEach(e => {
+            if (e.x >= 229 && e.y <= 732 && e.y >= 120 && e.x <= 773) {
+                accept_position.push(e);
+            }
+        });
+        return accept_position;
+    }
+
+    if (name == "phao_do1" && kind == "r") {
+        var default_position = new Array();
+        default_position.push(
+            //dọc đi lên 
+            new Position(x, y - 68),
+            new Position(x, y - 136),
+            new Position(x, y - 204),
+            new Position(x, y - 272),
+            new Position(x, y - 340),
+            new Position(x, y - 408),
+            new Position(x, y - 476),
+            new Position(x, y - 544),
+            new Position(x, y - 612),
+            //dọc đi xuống
+            new Position(x, y + 68),
+            new Position(x, y + 136),
+            new Position(x, y + 204),
+            new Position(x, y + 272),
+            new Position(x, y + 340),
+            new Position(x, y + 408),
+            new Position(x, y + 476),
+            new Position(x, y + 544),
+            new Position(x, y + 612),
+            //ngang qua trái
+            new Position(x - 68, y),
+            new Position(x - 136, y),
+            new Position(x - 204, y),
+            new Position(x - 272, y),
+            new Position(x - 340, y),
+            new Position(x - 408, y),
+            new Position(x - 476, y),
+            new Position(x - 544, y),
+            //ngang qua phải
+            new Position(x + 68, y),
+            new Position(x + 136, y),
+            new Position(x + 204, y),
+            new Position(x + 272, y),
+            new Position(x + 340, y),
+            new Position(x + 408, y),
+            new Position(x + 476, y),
+            new Position(x + 544, y)
+        );
+        default_position.forEach(e => {
+            if (e.x >= 229 && e.y <= 732 && e.y >= 120 && e.x <= 773) {
+                accept_position.push(e);
+            }
+        });
+        return accept_position;
     }
 
     //Co den
     if (name == "tot_den" && kind == "b") {
-        var default_Position = new Array();
-        default_Position.push(
+        var default_position = new Array();
+        default_position.push(
             new Position(x, y + 68)
         );
-        default_Position.forEach(e => {
+        default_position.forEach(e => {
             if (e.y > 460) {
-                accept_Position.push(e);
-                accept_Position.push(new Position(x + 68, y));
-                accept_Position.push(new Position(x - 68, y));
+                accept_position.push(e);
+                accept_position.push(new Position(x + 68, y));
+                accept_position.push(new Position(x - 68, y));
             }
             else {
-                accept_Position.push(e);
+                accept_position.push(e);
             }
         });
-        return accept_Position;
+        return accept_position;
     }
     if (name == "voi_den1" && kind == "b") {
-        var default_Position = new Array();
-        default_Position.push(
+        var default_position = new Array();
+        default_position.push(
             new Position(x + 136, y + 136),
             new Position(x - 136, y + 136),
             new Position(x - 136, y - 136),
             new Position(x + 136, y - 136)
         );
-        default_Position.forEach(e => {
+        default_position.forEach(e => {
             if (e.y < 275) {
-                accept_Position.push(e);
+                accept_position.push(e);
             }
         });
-        return accept_Position;
+        return accept_position;
     }
     if (name == "ma_den1" && kind == "b") {
-        var default_Position = new Array();
-        default_Position.push(
+        var default_position = new Array();
+        default_position.push(
             new Position(x - 68, y - 136),
             new Position(x + 68, y - 136),
             new Position(x - 136, y - 68),
@@ -158,97 +267,44 @@
             new Position(x - 68, y + 136),
             new Position(x + 68, y + 136),
         );
-        default_Position.forEach(e => {
+        default_position.forEach(e => {
             if (true) {
-                accept_Position.push(new Position(x + 68, y));
+                accept_position.push(new Position(x + 68, y));
             }
         });
-        return accept_Position;
+        return accept_position;
     }
     if (name == "tuong" && kind == "b") {
-        var default_Position = new Array();
-        default_Position.push(
+        var default_position = new Array();
+        default_position.push(
             new Position(x, y + 68),
             new Position(x, y - 68),
             new Position(x - 68, y),
             new Position(x + 68, y)
         );
-        default_Position.forEach(e => {
+        default_position.forEach(e => {
             if (e.y < 140) {
-                accept_Position.push(e);
+                accept_position.push(e);
             }
         });
-        return accept_Position;
+        return accept_position;
     }
     if (name == "si_den1" && kind == "b") {
-        var default_Position = new Array();
-        default_Position.push(
+        var default_position = new Array();
+        default_position.push(
             new Position(x - 68, y + 68),
             new Position(x + 68, y + 68),
             new Position(x - 68, y - 68),
             new Position(x + 68, y - 68)
         );
-        default_Position.forEach(e => {
+        default_position.forEach(e => {
             if (e.y < 150) {
-                accept_Position.push(e);
+                accept_position.push(e);
             }
         });
-        return accept_Position;
+        return accept_position;
     }
-    if (name == "xe_do1" && kind == "r") {
-        var default_Position = new Array();
-        default_Position.push(
-            //dọc đi lên 
-            //new Position(x, y - 68),
-            //new Position(x, y - 136),
-            //new Position(x, y - 204),
-            //new Position(x, y - 272),
-            //new Position(x, y - 340),
-            //new Position(x, y - 408),
-            //new Position(x, y - 476),
-            //new Position(x, y - 544),
-            //new Position(x, y - 612),
-
-            new Position(x, y - 68),
-            new Position(x, y - 136),
-            new Position(x, y - 204),
-            new Position(x + 68, y - 68)
-            ////dọc đi xuống
-            //new Position(x, y + 68),
-            //new Position(x, y + 136),
-            //new Position(x, y + 204),
-            //new Position(x, y + 272),
-            //new Position(x, y + 340),
-            //new Position(x, y + 408),
-            //new Position(x, y + 476),
-            //new Position(x, y + 544),
-            //new Position(x, y + 612),
-            ////ngang qua trái
-            //new Position(y, x - 68),
-            //new Position(y, x - 136),
-            //new Position(y, x - 204),
-            //new Position(y, x - 272),
-            //new Position(y, x - 340),
-            //new Position(y, x - 408),
-            //new Position(y, x - 476),
-            //new Position(y, x - 544),
-            ////ngang qua phải
-            //new Position(y, x + 68),
-            //new Position(y, x + 136),
-            //new Position(y, x + 204),
-            //new Position(y, x + 272),
-            //new Position(y, x + 340),
-            //new Position(y, x + 408),
-            //new Position(y, x + 476),
-            //new Position(y, x + 544)
-        );
-        default_Position.forEach(e => {
-            if (true) {
-                accept_Position.push(e);
-            }
-        });
-        return accept_Position;
-    }
+    
 }
 
 function findimage(a, b) {
